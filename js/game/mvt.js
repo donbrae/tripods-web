ff667548ca41007a7d6fb471885056280683a980
donbrae <jamie@donbrae.co.uk>
2021-04-16 19:45:56 +0100

    Remove shadow on jump due to WebKit issues.

--- js/game/mvt.js
+++ js/game/mvt.js
@@ -198,8 +198,9 @@ TRIPODS.mvt = (function (_module) {
             this.measurements.cells_in_row = _module.levels[_module.game_state.level][1].length;
             this.measurements.cells_in_column = _module.levels[_module.game_state.level].length - 1;
             this.measurements.jump_blur = `${cell / 20}px`;
-            this.measurements.jump_shadow_initial = "0 0 0 rgba(0, 0, 0, 0.25)";
-            this.measurements.jump_shadow_halfway = `${cell / 6}px ${cell / 6}px ${cell / 40}px rgba(0, 0, 0, 0.15)`;
+            // WebKit having performance issues rendering shadow (along with blur) on jump, so commented out for now. To restore, add below variables to `filter` property in relevant [keyframe] arrays
+            // this.measurements.jump_shadow_initial = "0 0 0 rgba(0, 0, 0, 0.25)";
+            // this.measurements.jump_shadow_halfway = `${cell / 6}px ${cell / 6}px ${cell / 40}px rgba(0, 0, 0, 0.15)`;
         }
     }
 
@@ -773,9 +774,9 @@ TRIPODS.mvt = (function (_module) {
             const translate_xy = _module.utils.getTranslateXY(foot);
 
             const keyframes = [
-                { transform: `translate(${translate_xy.tX}px,${translate_xy.tY}px)`, filter: `blur(0) drop-shadow(${_this.measurements.jump_shadow_initial})` },
-                { transform: `translate(${translate_xy.tX + x_shift / 2}px,${translate_xy.tY + y_shift / 2}px) scale(1.8)`, filter: `blur(${_this.measurements.jump_blur}) drop-shadow(${_this.measurements.jump_shadow_halfway})` }, // Halfway
-                { transform: `translate(${translate_xy.tX + x_shift}px,${translate_xy.tY + y_shift}px) scale(1)`, filter: `blur(0) drop-shadow(${_this.measurements.jump_shadow_initial})` }
+                { transform: `translate(${translate_xy.tX}px,${translate_xy.tY}px)`, filter: `blur(0)` },
+                { transform: `translate(${translate_xy.tX + x_shift / 2}px,${translate_xy.tY + y_shift / 2}px) scale(1.8)`, filter: `blur(${_this.measurements.jump_blur})` }, // Halfway
+                { transform: `translate(${translate_xy.tX + x_shift}px,${translate_xy.tY + y_shift}px) scale(1)`, filter: `blur(0)` }
             ];
 
             _module.utils.animate(foot, keyframes, { duration: _module.cfg.animation.jump_duration }, callback);
@@ -848,9 +849,9 @@ TRIPODS.mvt = (function (_module) {
             }
 
             const keyframes = [
-                { transform: `translate(${translate_xy.tX}px,${translate_xy.tY}px)`, filter: `blur(0) drop-shadow(${_this.measurements.jump_shadow_initial})` },
-                { transform: `translate(${translate_xy.tX + x_shift / 2 + x_shift_additional}px,${translate_xy.tY + y_shift / 2 + y_shift_additional}px) scale(1.5)`, filter: `blur(${_this.measurements.jump_blur}) drop-shadow(${_this.measurements.jump_shadow_halfway})` }, // Halfway
-                { transform: `translate(${translate_xy.tX}px,${translate_xy.tY}px) scale(1)`, filter: `blur(0) drop-shadow(${_this.measurements.jump_shadow_initial})` } // Back to original position
+                { transform: `translate(${translate_xy.tX}px,${translate_xy.tY}px)`, filter: `blur(0)` },
+                { transform: `translate(${translate_xy.tX + x_shift / 2 + x_shift_additional}px,${translate_xy.tY + y_shift / 2 + y_shift_additional}px) scale(1.5)`, filter: `blur(${_this.measurements.jump_blur})` }, // Halfway
+                { transform: `translate(${translate_xy.tX}px,${translate_xy.tY}px) scale(1)`, filter: `blur(0)` } // Back to original position
             ];
 
             const duration_additional = !x_shift_additional && !y_shift_additional ? 1 : (Math.abs(x_shift_additional + y_shift_additional) / foot_rect.width) * _module.cfg.animation.jump_duration * 0.3;
@@ -868,18 +869,18 @@ TRIPODS.mvt = (function (_module) {
             const translate_xy = _module.utils.getTranslateXY(foot);
 
             let keyframes = [
-                { transform: `translate(${translate_xy.tX}px,${translate_xy.tY}px)`, filter: `blur(0) drop-shadow(${_this.measurements.jump_shadow_initial})` }, // Initial
-                { transform: `translate(${translate_xy.tX + x_shift / 2}px,${translate_xy.tY + y_shift / 2}px) scale(1.8)`, filter: `blur(${_this.measurements.jump_blur}) drop-shadow(${_this.measurements.jump_shadow_halfway})` }, // Halfway between initial and block
-                { transform: `translate(${translate_xy.tX + x_shift}px,${translate_xy.tY + y_shift}px) scale(1)`, filter: `blur(0) drop-shadow(${_this.measurements.jump_shadow_initial})` }, // Block position
+                { transform: `translate(${translate_xy.tX}px,${translate_xy.tY}px)`, filter: `blur(0)` }, // Initial
+                { transform: `translate(${translate_xy.tX + x_shift / 2}px,${translate_xy.tY + y_shift / 2}px) scale(1.8)`, filter: `blur(${_this.measurements.jump_blur})` }, // Halfway between initial and block
+                { transform: `translate(${translate_xy.tX + x_shift}px,${translate_xy.tY + y_shift}px) scale(1)`, filter: `blur(0)` }, // Block position
             ];
 
             _module.utils.animate(foot, keyframes, { duration: _module.cfg.animation.jump_duration }, () => {
                 setTimeout(() => {
 
                     let keyframes = [
-                        { transform: `translate(${translate_xy.tX + x_shift}px,${translate_xy.tY + y_shift}px) scale(1)`, filter: `blur(0) drop-shadow(${_this.measurements.jump_shadow_initial})` }, // Block position
-                        { transform: `translate(${translate_xy.tX + x_shift / 2}px,${translate_xy.tY + y_shift / 2}px) scale(1.8)`, filter: `blur(${_this.measurements.jump_blur}) drop-shadow(${_this.measurements.jump_shadow_halfway})` }, // Halfway between initial and block
-                        { transform: `translate(${translate_xy.tX}px,${translate_xy.tY}px)`, filter: `blur(0) drop-shadow(${_this.measurements.jump_shadow_initial})` }, // Initial
+                        { transform: `translate(${translate_xy.tX + x_shift}px,${translate_xy.tY + y_shift}px) scale(1)`, filter: `blur(0)` }, // Block position
+                        { transform: `translate(${translate_xy.tX + x_shift / 2}px,${translate_xy.tY + y_shift / 2}px) scale(1.8)`, filter: `blur(${_this.measurements.jump_blur})` }, // Halfway between initial and block
+                        { transform: `translate(${translate_xy.tX}px,${translate_xy.tY}px)`, filter: `blur(0)` }, // Initial
 
                     ];
 
@@ -904,9 +905,9 @@ TRIPODS.mvt = (function (_module) {
             const translate_xy = _module.utils.getTranslateXY(foot);
 
             let keyframes = [
-                { transform: `translate(${translate_xy.tX}px,${translate_xy.tY}px)`, filter: `blur(0) drop-shadow(${_this.measurements.jump_shadow_initial})` }, // Initial
-                { transform: `translate(${translate_xy.tX + x_shift / 2}px,${translate_xy.tY + y_shift / 2}px) scale(1.8)`, filter: `blur(${_this.measurements.jump_blur}) drop-shadow(${_this.measurements.jump_shadow_halfway})` }, // Halfway between initial and block
-                { transform: `translate(${translate_xy.tX + x_shift}px,${translate_xy.tY + y_shift}px) scale(1)`, filter: `blur(0) drop-shadow(${_this.measurements.jump_shadow_initial})` }, // Block position
+                { transform: `translate(${translate_xy.tX}px,${translate_xy.tY}px)`, filter: `blur(0)` }, // Initial
+                { transform: `translate(${translate_xy.tX + x_shift / 2}px,${translate_xy.tY + y_shift / 2}px) scale(1.8)`, filter: `blur(${_this.measurements.jump_blur})` }, // Halfway between initial and block
+                { transform: `translate(${translate_xy.tX + x_shift}px,${translate_xy.tY + y_shift}px) scale(1)`, filter: `blur(0)` }, // Block position
             ];
 
             _module.utils.animate(foot, keyframes, { duration: _module.cfg.animation.jump_duration }, () => {
