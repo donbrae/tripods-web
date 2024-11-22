55aafe6176698063ae9fa35afa6fc1518597b902
donbrae <jamie@donbrae.co.uk>
2024-11-22 12:14:12 +0000

    Incorporate in-game title into build script.

--- build.sh
+++ build.sh
@@ -4,9 +4,11 @@ rm -r dist/css
 rm -r dist/js
 rm -r dist/sound
 rm -r dist/fonts
+rm -r dist/img
 cp -r css dist
 cp -r js dist
 cp -r sound dist
 cp -r fonts dist
+cp -r img dist
 babel dist/js/game/*.js -d dist/js/game
 exit 0
